7e4293ca0714ed14af926e39241cdfed0aed4631
Thomas Jerard <74585794+thomasjerard@users.noreply.github.com>
2025-07-01 02:25:17 +0530

    initial version of website

---
+++ src/components/Footbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FiPhone } from "react-icons/fi";
+import { MdOutlineLocationOn, MdOutlinePermPhoneMsg, MdOutlineMail } from "react-icons/md";
+
+function Footbar(){
+  return <section id='contact-us'>
+              <div id='footer'>
+                  <h1>Contact Us</h1>
+                  <h3>Feel free to contact us anytime—WhatsApp is our preferred way to connect!</h3>
+                  <div className='contacts-container'>
+                      <div className='contacts-grid'>
+                          <div className='flex-contact'>
+                              <FiPhone/>
+                              <p>04772 970297</p>
+                          </div>
+                          <div className='flex-contact'>
+                              <MdOutlineMail/>
+                              <p>drtijoalex@yahoo.co.in</p>
+                          </div>
+                          <div className='flex-contact'>
+                              <MdOutlinePermPhoneMsg/>
+                              <p>9847048002</p>
+                          </div>
+                          <div className='flex-contact'>
+                              <MdOutlineMail/>
+                              <p>info@drtijoalexorthodonticcentre.com</p>
+                          </div>
+                      </div>
+                      <div className='flex-contact' id='contact-address'>
+                          <MdOutlineLocationOn/>
+                          <p>Thevarcad Building <br/> North of Vazhicherry <br/> Bridge, Alapuzha - 688001</p>
+                      </div>
+                  </div>
+              </div>
+          </section>   
+}
+
+export default Footbar;
