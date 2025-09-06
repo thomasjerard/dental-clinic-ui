afb97c7044e16249cb5316140091fe9ed243e063
Thomas Jerard <74585794+thomasjerard@users.noreply.github.com>
2025-09-07 01:36:57 +0530

    updated gh pages latest commits to master branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import MainButton from './ContactUsButton';
 import { MdHomeFilled, MdMedicalServices, MdInfo } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
+import dentalClinic from "../assets/dental-clinic.png";
 import { NavLink } from "react-router"
 
-function Navbar(){
+function Navbar({setMenuBar, isMenuBarOpen}){
 
-    const [isMenuBarOpen, setMenuBar] = useState(false);
     const [activeClass, setActiveClass] = useState('navbar-buttons')
 
     const handleClick = () => {
@@ -24,13 +24,13 @@ function Navbar(){
 
 
     return <div id="navbar">
-        <NavLink to="/"><img src='./logo.gif' alt="Logo" /></NavLink>
+        <NavLink to="/"><img src={dentalClinic} alt="Logo" /></NavLink>
         <button onClick={handleClick} id='hamburger-menu'><RxHamburgerMenu/></button>
         <div className={activeClass} >
-            <NavLink to="/"><MdHomeFilled className='icon'/><p>Home</p></NavLink>
-            <NavLink to="/#services-section"><MdMedicalServices className='icon'/><p>Services</p></NavLink>
-            <NavLink to="/our_team#about-us-page"><MdInfo className='icon'/><p>About Us</p></NavLink>
-            <MainButton link="/#contact-us" theme="blue" title="Contact Us"/>
+            <NavLink to="/" onClick={handleClick}><MdHomeFilled className='icon'/><p>Home</p></NavLink>
+            <NavLink to="/#services-section" onClick={handleClick}><MdMedicalServices className='icon'/><p>Services</p></NavLink>
+            <NavLink to="/our_team#about-us-page" onClick={handleClick}><MdInfo className='icon'/><p>About Us</p></NavLink>
+            <MainButton onClick={handleClick} link="/#contact-us" theme="blue" title="Contact Us"/>
         </div>
     </div>;
 }
